23abda69350de3657b0717a32ac0e34b96d6bde3
Danielru <danielru@MacBook-Pro-de-Daniel.local>
2022-11-24 22:48:20 -0500

    zaa

--- pages/Maestro-historico/index.js
+++ pages/Maestro-historico/index.js
@@ -108,14 +108,14 @@ const Index = () => {
   ]
   
   const fileType = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
-    const fileExtension = '.xlsx';
+    const fileExtension = '.xls';
 
     const exportToCSV = (a) => {
         const ws = XLSX.utils.json_to_sheet(a);
         const wb = { Sheets: { 'data': ws }, SheetNames: ['data'] };
         const excelBuffer = XLSX.write(wb, { bookType: 'xlsx', type: 'array' });
         const data = new Blob([excelBuffer], {type: fileType});
-        FileSaver.saveAs(data, 'xx' + fileExtension);
+        FileSaver.saveAs(data, 'xx1' + fileExtension);
     }
 
  
